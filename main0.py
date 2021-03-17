8d0ac846199c0efd4c931759a1d041f2442f49f6
Alisa11110000 <79804290+Alisa11110000@users.noreply.github.com>
2021-03-17 23:16:01 +0300

    Add files via upload

---
+++ main0.py
@@ -0,0 +1,12 @@
+
+def print_fiogr(name):
+    print('Студент: ' + name)
+
+def print_gr(name):
+    print('Группа: ' + name)
+
+if __name__ == '__main__':
+    print_fiogr('Вдовина Алиса Владиславовна')
+    print_gr('РКТ2-81')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
