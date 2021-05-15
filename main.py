7dc6978ee7488c8901c72a387925a46154b7813d
Alisa11110000 <79804290+Alisa11110000@users.noreply.github.com>
2021-05-15 14:28:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -153,36 +153,25 @@ def peremecheniya(e, f, q, N_e, n_e, n_e_k):
     return U
 
 def scalar(a,b):
-    """Скалярное произведение"""
     return a[0]*b[0]+a[1]*b[1]
 
 def peremech_elem_x(e, U, num_el, x, l=1):
-    """Перемещение в координате x элемента с номером num_el"""
-    # проверка на входные данные
-    # номер элемента не должен быть меньше 1 и больше количетсва элементов
     if num_el < 1 or num_el > e:
         print("Неверный номер номер элемента")
-        exit(1) # Выходим с ошибкой
+        exit(1)
     N1 = 1 - x / l
     N2 = x / l
     N = [N1, N2]
-
-    # Получаем перемещения с крайних узлов элемента
-    # Левый узел
     u1 = U[num_el-1]
-    # Правый узел
     u2 = U[num_el]
     u = [u1, u2]
-    # Значение перемещение в координате x
     return scalar(N, u)
 
 
 def peremech_elem(e, U, num_el, l=1):
     print("Элемент №", num_el)
     for i in range(5):
-        # Получаем координату
         coord = 0.25*i*l
-        # Находим перемещение в координате
         u = peremech_elem_x(e, U, num_el, coord, l=l)
         print(f'x={0.25*i:4.2f} u={u:6.3f}')
 
@@ -190,7 +179,7 @@ def peremech_elem(e, U, num_el, l=1):
 def vnut_usil(e, U, num_el, x, l=1):
     if num_el < 1 or num_el > e:
         print("Неверный номер номер элемента")
-        exit(1)  # Выходим с ошибкой
+        exit(1)
     EF_K = kolvo_elementov_i_gestkost_kagdogo_elementa(e)
     N1_proiz = - 1 / l
     N2_proiz = 1 / l
@@ -204,9 +193,7 @@ def vnut_usil(e, U, num_el, x, l=1):
 def vnut_ysiliya(e, U, num_el, l=1):
     print("Элемент №", num_el)
     for i in range(5):
-        # Получаем координату
         coord = 0.25 * i * l
-        # Находим усилие в координате
         N_elem = vnut_usil(e, U, num_el, coord, l=1)
         print(f'x={0.25 * i:4.2f} N_elem={N_elem:6.3f}')
 
@@ -227,17 +214,12 @@ if __name__ == '__main__':
     silovaya_nagryzka(e, f, q, N_e, n_e, n_e_k)
     U = peremecheniya(e, f, q, N_e, n_e, n_e_k)
     print(U)
-    # приведём к массиву
     U = [x[0] for x in U]
     print('Суммарный вектор перемещений')
     print(U)
-
-    # Выводим апроксимации для всех элементов
     for num_el in range(1, e+1):
-        # визуальный разграничитель для апроксимаций
         print('-'*30)
         peremech_elem(e, U, num_el, l=1)
-
     for num_el in range(1, e+1):
         print('Усилия в элементе:')
         print('-' * 30)
